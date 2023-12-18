623c57040301fdde13b09e54155d74afb174a5ec
Sos-V <sosyan.vahe@gmail.com>
2023-12-18 21:17:11 +0100

    css styling is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [persons, setPersons] = useState();
+  const [name, setName] = useState(null);
+  const [clicked, setClicked] = useState(false);
+  const [age, setAge] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:9090/persons")
+      .then(res => res.json())
+      .then(data => setPersons(data), console.log(persons))
+
+  }, [clicked])
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:9090/persons', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "name": name, "age": age }),
+      });
+
+      if (response.ok) {
+        console.log('Data submitted successfully');
+      } else {
+        console.error('Failed to submit data');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => clicked? setClicked(false) : setClicked(true)}>GetAllPersons</button>
+      <form className="Form" onSubmit={onSubmit}>
+
+        <div className="control">
+          <label htmlFor="name">Name:</label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            name="name"
+            id="name"
+          />
+        </div>
+        <div color="red">
+          <label htmlFor="age">Age:</label>
+          <input
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            age="name"
+            id="age"
+          />
+        </div>
+        <button >save</button>
+      </form >
+    </>
   );
-}
+};
 
 export default App;
